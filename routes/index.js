f43d39779a42e49643478a75578639d98e22d043
Anton Hörl <anton.hoerl@gmx.at>
2017-08-04 22:57:51 +0200

    Changed cronTime split functionality

--- routes/index.js
+++ routes/index.js
@@ -221,29 +221,32 @@ router.post('/guestsMessage', function(req, res, next) {
                             var bufferObject = JSON.parse(buffer);
 
                             //console.log(bufferObject);
-                            console.log("jobcoirntime: " + job.cronTime.toString());
-                            console.log("jobcoirntime: " + job.cronTime.toString().slice(2, 4));
+                            var cronTimeSplitted = job.cronTime.split(" ");
 
-                            var minutes = job.cronTime.toString().slice(2, 4);
+                            console.log("jobcrontime splitted: " + cronTimeSplitted);
+
+                            var minutes = cronTimeSplitted[0];
                             if (minutes.length === 1) {
                                 minutes = "0" + minutes;
                             }
-                            var hour = job.cronTime.toString().slice(5, 7);
+                            var hour = cronTimeSplitted[1];
                             if (hour.length === 1) {
                                 hour = "0" + hour;
                             }
-                            var day = job.cronTime.toString().slice(8, 10);
+                            var day = cronTimeSplitted[2];
                             if (day.length === 1) {
                                 day = "0" + day;
                             }
-                            var monthNumber = job.cronTime.toString().slice(11, 12);
-                            console.log("---->>>monthnumber"+monthNumber);
+                            var monthNumber = cronTimeSplitted[3];
+
+                            console.log("---->>>monthnumber" + monthNumber);
 
                             var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
                                 "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
                             var month = monthNames[monthNumber];
-                            console.log("---->>>month"+monthNumber);
+
+                            console.log("---->>>month" + monthNames[monthNumber]);
                             //Filter the right message
                             var regex = String(month + " " + day + " 2017 " + hour + ":" + minutes);
                             console.log("---->regex:"+regex);
